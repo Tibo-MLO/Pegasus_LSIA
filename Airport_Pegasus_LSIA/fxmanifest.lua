af67481b0510fd36efdd6ec264a49bbf24c29ccd
Tibo MLO <135160241+Tibo-MLO@users.noreply.github.com>
2024-05-29 19:37:27 +0200

    Test Release

---
+++ Airport_Pegasus_LSIA/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+game 'gta5'
+author 'Tibo_MLo'
+description 'Airport LSIA'
+this_is_a_map 'yes'
+replace_level_meta 'gta5'
+
+file{
+    'gta5.meta',
+    'doortuning.ymt'
+}
+
+
+dependencies { 
+    '/server:4960',
+    '/gameBuild:2372',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/**.ytyp'
